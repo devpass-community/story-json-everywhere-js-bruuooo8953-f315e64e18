e234eb3258a9fbc26dfe5821dabdea32b36aa0a8
Bruno Luis <bruno.dev07@gmail.com>
2023-05-18 09:07:09 -0300

    My second commit

--- productService.js
+++ productService.js
@@ -1,6 +1,6 @@
 async function getProduct(productId) {
     const apiUrl = `https://fakestoreapi.com/products/${productId}`;
-    const product = apiUrl.json()
+    const product = apiUrl.json({})
     return product
 }
 
